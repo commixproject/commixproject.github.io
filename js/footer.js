62fd6d1c8c0045f33a1b50159a514fe6640ce344
stasinopoulos <an.stasinopoulos@gmail.com>
2021-03-27 10:25:36 +0200

    Minor update

---
+++ js/footer.js
@@ -0,0 +1,31 @@
+class Footer extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <footer id="ubea-footer" role="contentinfo">
+      <div class="ubea-container">
+        <div class="row copyright">
+          <div class="col-md-10">
+            <p class="pull-left">
+              <small class="block">
+                <br>
+                Made in Greece with <font size="2" color="grey"><b> <i class="fa fa-coffee"></b></i></font> and <font size="2" color="red"><b><i class="fa fa-heart"></i></b></font></font> by <a href="https://github.com/stasinopoulos" target="_blank">Anastasios Stasinopoulos</a>.
+                <br>
+                <a href="https://github.com/commixproject/" target="_blank">Commix Project</a> is licensed under the <a href="https://github.com/commixproject/commix/wiki/License" target="_blank">GPLv3 License</a> © 2014-2021.
+              </small>
+            </p>
+            <p class="pull-right">
+            </p>
+          </div>
+        </div>
+
+      </div>
+    </footer>
+    `;
+  }
+}
+
+customElements.define('footer-component', Footer);
---
+++ js/header.js
@@ -0,0 +1,31 @@
+class Header extends HTMLElement {
+  constructor() {
+    super();
+  }
+
+  connectedCallback() {
+    this.innerHTML = `
+    <div class="ubea-loader"></div>
+    <div id="page">
+    <nav class="ubea-nav" role="navigation">
+      <div class="ubea-container">
+        <div class="row">
+          <div class="col-sm-2 col-xs-12">
+            <div id="ubea-logo"><a href="index.html"><img src="https://commixproject.com/images/logo.png"></a>
+            </div>
+          </div>
+          <div class="col-xs-20 text-right main-nav menu-1 fixed">
+            <ul>
+              <li><a href="about" data-nav-section="about">ABOUT</a><font color="red"> | </font></li>
+              <li><a href="faq" data-nav-section="faq">FAQ</a><font color="red"> | </font></li>
+              <li><a target="_blank" href="https://www.paypal.com/donate/?hosted_button_id=UGBDUDJRW8U4E" class="external">DONATE</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+    `;
+  }
+}
+
+customElements.define('header-component', Header);
