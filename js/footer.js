d1f90c7a29fbbf5c08fa63d5a56acc01d62cc667
stasinopoulos <an.stasinopoulos@gmail.com>
2026-06-21 11:42:09 +0300

    Trivial update

--- js/footer.js
+++ js/footer.js
@@ -1,31 +1,69 @@
 class Footer extends HTMLElement {
-  constructor() {
-    super();
-  }
-
   connectedCallback() {
     this.innerHTML = `
-    <footer id="ubea-footer" role="contentinfo">
-      <div class="ubea-container">
-        <div class="row copyright">
-          <div class="col-md-12">
-            <p class="pull-left">
-              <small class="block">
+      <footer id="ubea-footer" role="contentinfo">
+        <div class="ubea-container">
+          <div class="row copyright">
+            <div class="col-md-12">
+
+              <p class="pull-left">
+                <small class="block">
+                  <br>
+                  Made in Greece with
+                  <i class="fa fa-coffee text-muted" aria-hidden="true"></i>
+                  and
+                  <i class="fa fa-heart text-danger" aria-hidden="true"></i>
+                  by
+                  <a
+                    href="https://github.com/stasinopoulos"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Anastasios Stasinopoulos
+                  </a>.
+                  <br>
+
+                  <a
+                    href="https://github.com/commixproject/"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    Commix Project
+                  </a>
+                  is licensed under the
+                  <a
+                    href="https://github.com/commixproject/commix/wiki/License"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    GPLv3 License
+                  </a>
+                  © 2014-2026.
+                </small>
+              </p>
+
+              <p class="pull-right">
                 <br>
-                Made in Greece with <font size="2" color="grey"><b> <i class="fa fa-coffee"></b></i></font> and <font size="2" color="red"><b><i class="fa fa-heart"></i></b></font></font> by <a href="https://github.com/stasinopoulos" target="_blank">Anastasios Stasinopoulos</a>.
-                <br>
-                <a href="https://github.com/commixproject/" target="_blank">Commix Project</a> is licensed under the <a href="https://github.com/commixproject/commix/wiki/License" target="_blank">GPLv3 License</a> © 2014-2026.
-              </small>
-            </p>
-            <p class="pull-right">
-              <br>
-            	<a href="https://www.obrela.com?utm_source=commixproject&utm_medium=banner&utm_campaign=clickthrough" target="_blank"><img src="https://commixproject.com/images/OSI_Logo_SPONSORED.png" width=250 height=auto></a> 
-            </p>
+                <a
+                  href="https://www.obrela.com?utm_source=commixproject&utm_medium=banner&utm_campaign=clickthrough"
+                  target="_blank"
+                  rel="sponsored noopener noreferrer"
+                >
+                  <img
+                    src="https://commixproject.com/images/OSI_Logo_SPONSORED.png"
+                    width="250"
+                    alt="Obrela Security Industries Sponsor"
+                    loading="lazy"
+                  >
+                </a>
+              </p>
+
+            </div>
           </div>
         </div>
-      </div>
-    </footer>
+      </footer>
     `;
   }
 }
+
 customElements.define('footer-component', Footer);
--- js/header.js
+++ js/header.js
@@ -1,29 +1,60 @@
 class Header extends HTMLElement {
-  constructor() {
-    super();
-  }
-
   connectedCallback() {
     this.innerHTML = `
-    <div class="ubea-loader"></div>
-    <div id="page">
-    <nav class="ubea-nav" role="navigation">
-      <div class="ubea-container">
-        <div class="row">
-          <div class="col-sm-2 col-xs-12">
-            <div id="ubea-logo"><a href="/"><img src="https://commixproject.com/images/logo.png" width=250 height=auto></a>
+      <div class="ubea-loader"></div>
+
+      <div id="page">
+        <nav class="ubea-nav" role="navigation" aria-label="Main Navigation">
+          <div class="ubea-container">
+            <div class="row">
+
+              <div class="col-sm-2 col-xs-12">
+                <div id="ubea-logo">
+                  <a href="/" aria-label="Commix Homepage">
+                    <img
+                      src="https://commixproject.com/images/logo.png"
+                      width="250"
+                      alt="Commix Project Logo"
+                    >
+                  </a>
+                </div>
+              </div>
+
+              <div class="col-sm-10 col-xs-12 text-right main-nav menu-1 fixed">
+                <ul>
+
+                  <li>
+                    <a href="#about" data-nav-section="about">
+                      ABOUT
+                    </a>
+                    <span class="nav-divider">|</span>
+                  </li>
+
+                  <li>
+                    <a href="#faq" data-nav-section="faq">
+                      FAQ
+                    </a>
+                    <span class="nav-divider">|</span>
+                  </li>
+
+                  <li>
+                    <a
+                      href="https://www.paypal.com/donate/?hosted_button_id=UGBDUDJRW8U4E"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      class="external"
+                    >
+                      DONATE
+                    </a>
+                  </li>
+
+                </ul>
+              </div>
+
             </div>
           </div>
-          <div class="col-xs-20 text-right main-nav menu-1 fixed">
-            <ul>
-              <li><a href="about" data-nav-section="about">ABOUT</a><font color="red"> | </font></li>
-              <li><a href="faq" data-nav-section="faq">FAQ</a><font color="red"> | </font></li>
-              <li><a target="_blank" href="https://www.paypal.com/donate/?hosted_button_id=UGBDUDJRW8U4E" class="external">DONATE</a></li>
-            </ul>
-          </div>
-        </div>
+        </nav>
       </div>
-    </nav>
     `;
   }
 }
