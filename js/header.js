b50e0a4c35c5f6074cc0c02888850c7aa167eb20
stasinopoulos <an.stasinopoulos@gmail.com>
2026-09-03 20:16:30 +0300

    Trivial update

--- js/header.js
+++ js/header.js
@@ -5,22 +5,20 @@ class Header extends HTMLElement {
 
       <div id="page">
         <nav class="ubea-nav" role="navigation" aria-label="Main Navigation">
-          <div class="ubea-container">
-            <div class="row">
+          <div class="ubea-nav-inner">
 
-              <div class="col-sm-2 col-xs-12">
-                <div id="ubea-logo">
-                  <a href="/" aria-label="Commix Homepage">
-                    <img
-                      src="https://commixproject.com/images/logo.png"
-                      width="270"
-                      alt="Commix Project Logo"
-                    >
-                  </a>
-                </div>
+              <div id="ubea-logo">
+                <a href="/" aria-label="Commix Homepage">
+                  <img
+                    src="https://commixproject.com/images/logo.png"
+                    width="220"
+                    height="45"
+                    alt="Commix Project Logo"
+                  >
+                </a>
               </div>
-      
-              <div class="col-sm-10 col-xs-12 text-right main-nav menu-1 fixed">
+
+              <div class="text-right main-nav menu-1 fixed">
                 <ul>
                   <li>
                     <a href="#about" data-nav-section="about">
@@ -42,7 +40,7 @@ class Header extends HTMLElement {
                   </li>
 
                   <li>
-                    <a href="#" id="spreadTrigger" class="external">
+                    <a href="#" class="external js-spread-trigger">
                       SPREAD
                     </a>
                   </li>
@@ -50,7 +48,6 @@ class Header extends HTMLElement {
                 </ul>
               </div>
 
-            </div>
           </div>
         </nav>
       </div>
@@ -83,25 +80,27 @@ class Header extends HTMLElement {
       </div>
     `;
 
-    // bind event AFTER render
-    setTimeout(() => {
-      const trigger = this.querySelector("#spreadTrigger");
+    // delegated: the nav is cloned into the mobile off-canvas menu
+    document.addEventListener("click", (e) => {
+      const trigger = e.target.closest(".js-spread-trigger");
+      if (!trigger) return;
 
-      if (trigger) {
-        trigger.addEventListener("click", (e) => {
-          e.preventDefault();
+      e.preventDefault();
 
-          // Bootstrap modal (needs jQuery + bootstrap.js)
-          if (window.jQuery) {
-            $("#spreadModal").modal("show");
-          } else {
-            // fallback if no bootstrap
-            const modal = this.querySelector("#spreadModal");
-            if (modal) modal.style.display = "block";
-          }
-        });
+      // close the mobile off-canvas menu, if open
+      document.body.classList.remove("offcanvas");
+      document.querySelectorAll(".js-ubea-nav-toggle")
+        .forEach((el) => el.classList.remove("active"));
+
+      // Bootstrap modal (needs jQuery + bootstrap.js)
+      if (window.jQuery) {
+        window.jQuery("#spreadModal").modal("show");
+      } else {
+        // fallback if no bootstrap
+        const modal = this.querySelector("#spreadModal");
+        if (modal) modal.style.display = "block";
       }
-    }, 0);
+    });
   }
 }
 
